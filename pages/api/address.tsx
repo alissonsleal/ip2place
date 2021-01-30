4b571c958376a5140fb9923b718f40e8609c8b09
Alisson Leal <alissonsleal@hotmail.com>
2021-01-29 21:33:18 -0300

    cleanup

--- pages/api/address.tsx
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from "axios";
-
-export default async (req: any, res: any) => {
-  const { data } = await axios.get(`http://ip-api.com/json/`);
-  res.statusCode = 200;
-  res.json({ data: data });
-};
